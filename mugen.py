bab120b69f84549010d1c9924c2c629d634fce61
Can Berk Güder <cbguder@gmail.com>
2009-04-01 18:39:08 +0300

    Add Cohen-Sutherland algorithm.

--- mugen.py
+++ mugen.py
@@ -141,6 +141,66 @@ def interpolate(a, b, ratio):
 
 	return Point(None, x, y)
 
+# Cohen-Sutherland Algorithm
+def intersects(road, region):
+	LEFT, RIGHT, BOTTOM, TOP = 1, 2, 4, 8
+
+	xmin = region['x']
+	xmax = region['x'] + region['width']
+	ymin = region['y']
+	ymax = region['y'] + region['height']
+
+	def outcode(x, y):
+		code = 0
+
+		if y > ymax:
+			code += TOP
+		elif y < ymin:
+			code += BOTTOM
+
+		if x > xmax:
+			code += RIGHT
+		elif x < xmin:
+			code += LEFT
+
+		return code
+
+	x0, y0 = road.start.x, road.start.y
+	x1, y1 = road.end.x, road.end.y
+
+	start_code = outcode(x0, y0)
+	end_code = outcode(x1, y1)
+
+	while True:
+		if start_code | end_code == 0:
+			return True
+
+		if start_code & end_code != 0:
+			return False
+
+		max_code = max(start_code, end_code)
+
+		if max_code & TOP == TOP:
+			x = x0 + (x1 - x0) * (ymax - y0) / (y1 - y0)
+			y = ymax
+		elif max_code & BOTTOM == BOTTOM:
+			x = x0 + (x1 - x0) * (ymin - y0) / (y1 - y0)
+			y = ymin
+
+		if max_code & RIGHT == RIGHT:
+			y = y0 + (y1 - y0) * (xmax - x0) / (x1 - x0)
+			x = xmax
+		elif max_code & LEFT == LEFT:
+			y = y0 + (y1 - y0) * (xmin - x0) / (x1 - x0)
+			x = xmin
+
+		if max_code == start_code:
+			x0, y0 = x, y
+			start_code = outcode(x0, y0)
+		else:
+			x1, y1 = x, y
+			end_code = outcode(x1, y1)
+
 def plot_vehicle_count(f, vehicles):
 	v_count = {}
 
