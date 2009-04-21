3467bfc4d1508f02814b2821703dfb5d2fa08217
Can Berk Güder <cbguder@gmail.com>
2009-04-21 03:54:16 +0300

    Use Cairo to draw SVGs.

--- draw.py
+++ draw.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 
 import sys
+import math
+import cairo
 import PSDraw
 from parser import parse
+from util import intersect
+
+REGION_EXTEND = 100
+RANGE = 500
 
 def main():
 	if len(sys.argv) < 2:
@@ -10,35 +16,55 @@ def main():
 		sys.exit()
 
 	filename     = sys.argv[1]
-	out_filename = filename + '.ps'
+	out_filename = filename + '.svg'
 
 	f = open(filename)
 	draw(f, out_filename)
 	f.close()
 
 def draw(f, out):
-	(points, roads) = parse(f)
+	points, roads, regions = parse(f)
 
-	scale = 0
+	mX = max([p.x for p in points])
+	mY = max([p.y for p in points])
 
-	for p in points:
-		M = max(p.x, p.y)
-		if M > scale:
-			scale = M
+	surface = cairo.SVGSurface(out, mX, mY)
+	context = cairo.Context(surface)
+	context.set_line_width(2)
 
-	out = open(out, 'w')
-	ps = PSDraw.PSDraw(out)
-	ps.begin_document()
+	context.set_source_rgb(1, 1, 1)
+	context.rectangle(0, 0, mX, mY)
+	context.fill()
 
+	for region in regions:
+		context.set_source_rgba(0, 1, 0, 0.5)
+		context.rectangle(region['x'] - REGION_EXTEND, region['y'], REGION_EXTEND, region['height'])
+		context.fill()
+		context.rectangle(region['x'] + region['width'], region['y'], REGION_EXTEND, region['height'])
+		context.fill()
+
+		context.set_source_rgba(1, 0, 0, 0.5)
+		context.rectangle(region['x'], region['y'], region['width'], region['height'])
+		context.fill()
+
+	context.set_source_rgb(0, 0, 0)
 	for road in roads:
-		a = road.start.x * 600 / scale
-		b = road.start.y * 600 / scale
-		c = road.end.x * 600 / scale
-		d = road.end.y * 600 / scale
-		ps.line((a, b), (c, d))
+		context.move_to(road.start.x, road.start.y)
+		context.line_to(road.end.x, road.end.y)
+		context.stroke()
 
-	ps.end_document()
-	out.close()
+	context.set_source_rgb(0, 0, 1)
+	for region in regions:
+		for road in roads:
+			intersection = intersect(road, region)
+			if intersection != None:
+				context.arc(intersection[0], intersection[1], RANGE, 0, 2 * math.pi)
+				context.stroke()
+
+				context.arc(intersection[0], intersection[1], 2, 0, 2 * math.pi)
+				context.fill()
+
+	surface.finish()
 
 if __name__ == '__main__':
 	main()
